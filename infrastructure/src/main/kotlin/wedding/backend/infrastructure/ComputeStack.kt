f3ce06370fc8fce30f0adc693085520ef187d63f
Luke Durkan <ldurkan92@gmail.com>
2023-03-12 22:39:36 +1100

    Fixed buildspec artifact indentation, and added startup script for EC2.

--- infrastructure/src/main/kotlin/wedding/backend/infrastructure/ComputeStack.kt
+++ infrastructure/src/main/kotlin/wedding/backend/infrastructure/ComputeStack.kt
@@ -84,6 +84,20 @@ class ComputeStack(
                 .securityGroup(securityGroup)
                 .instanceType(InstanceType.of(InstanceClass.T2, InstanceSize.MICRO))
                 .machineImage(MachineImage.latestAmazonLinux())
+                .userData(UserData.custom(
+                    """
+                        sudo yum update
+                        sudo yum install ruby
+                        sudo yum install wget
+                        cd /home/ec2-user
+                        wget https://aws-codedeploy-ap-southeast-2.s3.ap-southeast-2.amazonaws.com/latest/install
+                        chmod +x ./install
+                        sudo ./install auto
+                        sudo yum install -y python-pip
+                        sudo pip install awscli
+                        sudo amazon-linux-extras install java-corretto17
+                    """.trimIndent()
+                ))
 //                        .keyName("simple-instance-1-key")
                 .healthCheck(HealthCheck.ec2())
                 .minCapacity(1)
