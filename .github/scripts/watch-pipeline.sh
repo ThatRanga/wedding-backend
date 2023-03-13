b0e5395a9a12cc2e76348d7e83a8942bbc95a13a
Luke Durkan <ldurkan92@gmail.com>
2023-03-13 17:12:37 +1100

    Changed equality check.

--- .github/scripts/watch-pipeline.sh
+++ .github/scripts/watch-pipeline.sh
@@ -3,7 +3,7 @@
 PIPELINE_ID=$1
 STATE=$(aws codepipeline get-pipeline-execution --pipeline-name DefaultPipeline --pipeline-execution-id "$PIPELINE_ID" | jq -e '.pipelineExecution.status')
 
-while [ "$STATE" = "InProgress" ]
+while [ "$STATE" == "InProgress" ]
 do
   sleep 10
   STATE=$(aws codepipeline get-pipeline-execution --pipeline-name DefaultPipeline --pipeline-execution-id "$PIPELINE_ID" | jq -e '.pipelineExecution.status')
