fd21cd7e7dc626825a957407c4ac702bb63cb737
Luke Durkan <ldurkan92@gmail.com>
2023-03-13 11:34:21 +1100

    Fixed privilege script deleting all files.

--- scripts/server/fix_privileges.sh
+++ scripts/server/fix_privileges.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-sudo rm -rf /home/ec2-user/server
+chmod +x /home/ec2-user/server/* jar
+chmod +x /home/ec2-user/server/server_start.sh
+chmod +x /home/ec2-user/server/server_stop.sh
