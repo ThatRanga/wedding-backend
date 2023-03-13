b8d59e8aa48d648b488c31f5f870a08cfa5a722e
Luke Durkan <ldurkan92@gmail.com>
2023-03-13 17:03:39 +1100

    Fixed pipeline script to escape quotes.

--- .github/scripts/start-pipeline.sh
+++ .github/scripts/start-pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pipelineId=$(aws codepipeline start-pipeline-execution --name DefaultPipeline | jq -e '.pipelineExecutionId')
+pipelineId=$(aws codepipeline start-pipeline-execution --name DefaultPipeline | jq -r -e '.pipelineExecutionId')
 
 echo "Pipeline started with id: $pipelineId"
 echo "$pipelineId" > artifact.txt
