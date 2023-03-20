fab9953ff9eb1210d571fe902ef8b45647cf3836
Luke Durkan <ldurkan92@gmail.com>
2023-03-20 20:56:59 +1100

    Adding cdk context to allow for vpc lookup.

--- infrastructure/src/main/kotlin/wedding/backend/infrastructure/ComputeStack.kt
+++ infrastructure/src/main/kotlin/wedding/backend/infrastructure/ComputeStack.kt
@@ -93,7 +93,7 @@ class ComputeStack(
                 .build()
         )
 
-        Tags.of(asg).add("test", "test5")
+        Tags.of(asg).add("test", "test6")
 
         val applicationTargetProps = AddApplicationTargetsProps.builder()
             .port(80)
