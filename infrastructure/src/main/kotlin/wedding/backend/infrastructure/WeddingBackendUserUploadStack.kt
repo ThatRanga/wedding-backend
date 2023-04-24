3a12b0f079bb14cd401635094b3e3f2b17253331
Luke Durkan <ldurkan92@gmail.com>
2023-04-24 16:00:35 +1000

    Updated policies to allow for s3 access by lambda.

--- infrastructure/src/main/kotlin/wedding/backend/infrastructure/WeddingBackendUserUploadStack.kt
+++ infrastructure/src/main/kotlin/wedding/backend/infrastructure/WeddingBackendUserUploadStack.kt
@@ -11,27 +11,54 @@ class WeddingBackendUserUploadStack(scope: Construct, id: String, env: String, v
     init {
         val bucket = WeddingBackendUserUploadS3Construct(this, env).bucket
 
-        val lambdaExecutionRole = Role(this, "lambda-execution-role", RoleProps.builder()
-            .assumedBy(ServicePrincipal("lambda.amazonaws.com"))
-            .inlinePolicies(mapOf(
-                "AllowS3Access" to PolicyDocument(PolicyDocumentProps.builder()
-                    .statements(listOf(
-                        PolicyStatement(PolicyStatementProps.builder()
-                            .actions(listOf("s3:GetObject"))
-                            .effect(Effect.ALLOW)
-                            .resources(listOf(bucket.bucketArn))
-                            .build()
+        val lambdaExecutionRole = Role(
+            this, "lambda-execution-role", RoleProps.builder()
+                .assumedBy(ServicePrincipal("lambda.amazonaws.com"))
+                .inlinePolicies(
+                    mapOf(
+                        "AllowS3Access" to PolicyDocument(
+                            PolicyDocumentProps.builder()
+                                .statements(
+                                    listOf(
+                                        PolicyStatement(
+                                            PolicyStatementProps.builder()
+                                                .actions(listOf("s3:GetObject"))
+                                                .effect(Effect.ALLOW)
+                                                .resources(listOf(
+                                                    bucket.bucketArn,
+                                                    "${bucket.bucketArn}/*"
+                                                ))
+                                                .build()
+                                        )
+                                    )
+                                )
+                                .build()
                         )
-                    ))
-                    .build()
+                    )
                 )
-            ))
-            .managedPolicies(listOf(
-                ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole")
-            ))
-            .build()
+                .managedPolicies(
+                    listOf(
+                        ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole")
+                    )
+                )
+                .build()
         )
 
         WeddingBackendUserUploadLambdaConstruct(this, vpc, bucket, lambdaExecutionRole)
+
+        bucket.addToResourcePolicy(
+            PolicyStatement(
+                PolicyStatementProps.builder()
+                    .actions(listOf("s3:GetObject"))
+                    .effect(Effect.ALLOW)
+                    .principals(listOf(ArnPrincipal(lambdaExecutionRole.roleArn)))
+                    .resources(listOf(
+                        bucket.bucketArn,
+                        "${bucket.bucketArn}/*"
+                    ))
+                    .build()
+            )
+
+        )
     }
 }
