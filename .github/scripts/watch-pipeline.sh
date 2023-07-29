b65cb64b66a1b59d53114665ac057b96e355c3e9
Luke Durkan <ldurkan92@gmail.com>
2023-07-29 19:30:09 +1000

    updated script to create log directory

--- .github/scripts/watch-pipeline.sh
+++ .github/scripts/watch-pipeline.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 PIPELINE_ID=$1
+ENV=$2
 echo "Pipeline ID = '$PIPELINE_ID'}"
-STATE=$(aws codepipeline get-pipeline-execution --pipeline-name "$env"-wedding-backend-pipeline --pipeline-execution-id "$PIPELINE_ID" | jq -r -e '.pipelineExecution.status')
+STATE=$(aws codepipeline get-pipeline-execution --pipeline-name "$ENV"-wedding-backend-pipeline --pipeline-execution-id "$PIPELINE_ID" | jq -r -e '.pipelineExecution.status')
 
 while [ "$STATE" = "InProgress" ]
 do
   sleep 10
-  STATE=$(aws codepipeline get-pipeline-execution --pipeline-name "$env"-wedding-backend-pipeline --pipeline-execution-id "$PIPELINE_ID" | jq -r -e '.pipelineExecution.status')
+  STATE=$(aws codepipeline get-pipeline-execution --pipeline-name "$ENV"-wedding-backend-pipeline --pipeline-execution-id "$PIPELINE_ID" | jq -r -e '.pipelineExecution.status')
 done
 
 if [ "$STATE" != "Succeeded" ] ; then
